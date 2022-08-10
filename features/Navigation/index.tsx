c4ce9b0160e32b03589d945d1ac5054edb1507ed
Leti <leticiapistarelli@gmail.com>
2022-08-10 17:26:57 -0300

    fix styles

--- features/Navigation/index.tsx
+++ features/Navigation/index.tsx
@@ -1,17 +1,31 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light" >
-      <div className='container-fluid'>
-          <Image src="/logo.png" alt='logo' width={50} height={50}/>
-        <Link href="/products"><a className='nav-link'>Home</a></Link>
-        <Link href="/products/offers"><a className='nav-link'>Sale</a></Link>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="navbar-brand" style={{ paddingLeft: "25px" }}>
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={50} height={50} />
+        </Link>
+      </div>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link href="/products">
+              <a className="nav-link">Home</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/products/offers">
+              <a className="nav-link">Sale</a>
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
