02d2f57a02e467cd62aeead542158383fde2a4a0
Leti <leticiapistarelli@gmail.com>
2022-08-10 16:32:13 -0300

    refactor 'fetch client-side' to 'server-side-rendering' in products/index.ts

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,32 +1,37 @@
-import { NextPage } from 'next'
-import React, { useState, useEffect } from 'react'
-import BookCard from '../../features/Book/BookCard'
-import styles from "../../styles/Products.module.css"
+import { GetServerSideProps, NextPage } from "next";
+import React, { useState, useEffect } from "react";
+import BookCard from "../../features/Book/BookCard";
+import styles from "../../styles/Products.module.css";
 
-
-export type Book = {name: string, id: string, image: string}
-export type Books = Book[]
-
-const Products: NextPage = () => {
-    const [books, setBooks] = useState<Books>([])
-
-    useEffect(() => {
-        fetch("http://localhost:3000/api/products")
-        .then((res) => res.json())
-        .then((data: Book[] )=> setBooks(data))
-    }, [books])
-    
-
-  return (
-    <main>
-        {/* //Stock
-        <BookCard/>  */}
-    </main>
-
-  )
+export type Book = { name: string; id: string; image: string };
+export type Books = Book[];
+interface Props {
+  books: Book[];
 }
 
-//Tienen predefinido styles.booksGrid para los libros 
+const Products: NextPage<Props> = ({ books }) => {
+  return (
+    <main>
+      <div className={styles.heading}>
+        <h2>Books store</h2>
+        <p>Titles available: {books.length}</p>
+      </div>
+      <section className={styles.booksGrid}>
+        {books.length &&
+          books.map((book) => <BookCard key={book.id} data={book} />)}
+      </section>
+    </main>
+  );
+};
 
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await fetch("http://localhost:3000/api/products");
+  const data: Book[] = await response.json();
+  return {
+    props: {
+      books: data,
+    },
+  };
+};
 
-export default Products
+export default Products;
