87221da67e5598ece1a868cb53d015abffc7af54
Leti <leticiapistarelli@gmail.com>
2022-08-10 16:54:43 -0300

    configure 'getStaticProps' render

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,49 +1,50 @@
-import { GetStaticPaths, GetStaticProps, GetServerSideProps } from 'next'
-import { useRouter } from 'next/router'
-import React, { FC } from 'react'
-import Book from '../../features/Book/BookCard'
-import BookInfo from '../../features/Book/BookInfo'
+import { GetStaticPaths, GetStaticProps, GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import React, { FC } from "react";
+import Book from "../../features/Book/BookCard";
+import BookInfo from "../../features/Book/BookInfo";
 
-type Book = {name: string, id: string, image: string}
+type Book = { name: string; id: string; image: string };
 
 interface Props {
-    book: Book
+  book: Book;
 }
 
-const Product: FC<Props> = ({book}) => {
-    const router = useRouter()
-    const handleBought = async()=>{
-        await fetch(`http://localhost:3000/api/products/${book.id}`, { method: 'DELETE' })
-        alert("Thanks for buying!")
-        router.push("/products")
-    }
+const Product: FC<Props> = ({ book }) => {
+  const router = useRouter();
+  const handleBought = async () => {
+    await fetch(`http://localhost:3000/api/products/${book.id}`, {
+      method: "DELETE",
+    });
+    alert("Thanks for buying!");
+    router.push("/products");
+  };
 
-  return (
-    <BookInfo book={book} handleBought={handleBought} />
-  )
-}
+  return <BookInfo book={book} handleBought={handleBought} />;
+};
 
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const res = await fetch(`http://localhost:3000/api/products/${params?.id}`);
+  const data: Book = await res.json();
 
-// export const getStaticProps: GetStaticProps =async ({params}) => {
-//     const res = await fetch(`http://localhost:3000/api/products/${params?.id}`)
-//     //...
-   
-// }
-// export const getServerSideProps: GetServerSideProps =async ({params}) => {
-//     const res = await fetch(`http://localhost:3000/api/products/${params?.id}`)
-//     //...
-// }
+  return {
+    props: {
+      book: data,
+    },
+  };
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const res = await fetch(`http://localhost:3000/api/products`)
-    const data: Book[] = await res.json()
-    const paths = data.map(book => {
-        return {params: {id: book.id}}})
+  const res = await fetch(`http://localhost:3000/api/products`);
+  const data: Book[] = await res.json();
+  const paths = data.map((book) => {
+    return { params: { id: book.id } };
+  });
 
-    return {
-        paths,
-        fallback: false
-    }
-    
-}
-export default Product
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export default Product;
